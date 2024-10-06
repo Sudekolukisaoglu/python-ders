8fd9c40bfe7774411463383f8def30b3d1a9c8f8
Sudekolukisaoglu <sudekolukisaoglu8@gmail.com>
2024-10-06 13:10:21 +0300

    Add files via upload

---
+++ python5.py
@@ -0,0 +1,17 @@
+meyveler = ["Elma","Muz","Nar","Dut"]
+çorbalar = ["Tarhana","Mercimek","Yayla"]
+
+print(meyveler)
+print(*meyveler)
+print(*meyveler, sep = ",")
+print(*meyveler, sep = "\n")
+
+print("Van","Muş","Çan","Bor")
+print("Van","Muş","Çan","Bor",sep=" ")
+print("Van","Muş","Çan","Bor",sep="-")
+
+print("Van","Muş","Çan","Bor",end="")
+print("Van","Muş","Çan","Bor",sep="-", end="")
+print("Şehirler")
+
+            
