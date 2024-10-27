b90bc63e959a5b787656b7c3e115b8bd9430f443
Your Name <sudekolukisaoglu8@gmail.com>
2024-10-27 10:07:01 +0300

    fr

---
+++ proje/çarpımtablosu.py
@@ -0,0 +1,7 @@
+def çarpım_tablosu():
+    print("ÇARPIM TABLOSU")
+    print("---------------")
+    for i in range(1, 11):
+        for j in range(1, 11):
+            print(f"{i * j:4}", end=' ')
+        print()  
