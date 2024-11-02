1e3b6fa829d0d881eb689f49cd2a42a42160ff5e
Your Name <sudekolukisaoglu8@gmail.com>
2024-11-02 10:32:11 +0300

    DERS

---
+++ python dersleri/h5_1/tekrar _if01.py
@@ -0,0 +1,11 @@
+a = 33
+b =200
+gün = "Pazar"
+# if b > a : print("true sonucu geldi.)
+# print (a < b)
+# if True : print ("deneme")
+# if a < b :
+#   print ("deneme")
+
+#  if gün == "Cuma" or gün == "Cumartesi":print("Yarın tatil")
+if True or False
