48b52ba2366d95f5339b7e5efe0d229f3860820c
Your Name <sudekolukisaoglu8@gmail.com>
2024-11-03 10:07:16 +0300

    python

---
+++ python dersleri/h5_1/tekrar_dosy1.py
@@ -0,0 +1,13 @@
+# open("deneme","r")
+# open("deneme","r").read()
+# okunan = open("deneme","r").read()
+
+# print(okunan)
+
+
+
+# ikinci yol kısa yol
+dosya = open("deneme","r")
+okunan = dosya.read()
+print(okunan)
+
