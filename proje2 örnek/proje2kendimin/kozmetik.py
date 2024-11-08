785a17344b5c218c2adac86ef31ad4f7eee61c95
Your Name <sudekolukisaoglu8@gmail.com>
2024-11-08 19:36:22 +0300

    proje

--- proje2 örnek/proje2kendimin/kozmetik.py
+++ proje2 örnek/proje2kendimin/kozmetik.py
@@ -1,38 +1,39 @@
 def urun_ekle():
-    dosya = open("kozmetik_urunler.txt", "a")
-    print("\n\nKozmetik ürüne ait bilgileri giriniz")
-    urun_adi = input("Ürün Adı: ")
-    fiyat = input("Fiyat: ")
-    kullanim_talimati = input("Kullanım Talimatı: ")
-    tanitim = input("Ürün Tanıtımı: ")09
-    dosya.write(f"{urun_adi}#{fiyat}#{kullanim_talimati}#{tanitim}\n")
-    dosya.close()
+    with open("kozmetik_urunler.txt", "a") as dosya:
+        print("\n\nKozmetik ürüne ait bilgileri giriniz")
+        urun_adi = input("Ürün Adı: ")
+        fiyat = input("Fiyat: ")
+        tanitim = input("Ürün Tanıtımı: ")
+        dosya.write(f"{urun_adi}#{fiyat}#{tanitim}\n")
     print("\n\nÜrün Listesi (güncel hali):")
     urun_listele()
 
 def urun_listele():
     print("\n\nÜrünler:")
-    dosya = open("kozmetik_urunler.txt")
-    urunler = dosya.readlines()
+    with open("kozmetik_urunler.txt") as dosya:
+        urunler = dosya.readlines()
     for sira, urun in enumerate(urunler):
         urun_bilgileri = urun.split("#")
-        print(f"{sira+1} - Ürün Adı: {urun_bilgileri[0]}, Fiyat: {urun_bilgileri[1]}, Kullanım Talimatı: {urun_bilgileri[2]}")
-    dosya.close()
+        print(f"{sira+1} - Ürün Adı: {urun_bilgileri[0]}, Fiyat: {urun_bilgileri[1]}")
 
 def urun_tanit():
     urun_listele()
     urun_no = int(input("Tanıtımını görmek istediğiniz ürün numarasını giriniz: ")) - 1
-    dosya = open("kozmetik_urunler.txt")
-    urunler = dosya.readlines()
-    dosya.close()
+    with open("kozmetik_urunler.txt") as dosya:
+        urunler = dosya.readlines()
     if 0 <= urun_no < len(urunler):
         urun_bilgileri = urunler[urun_no].split("#")
         print(f"\nÜrün Adı: {urun_bilgileri[0]}")
         print(f"Fiyat: {urun_bilgileri[1]}")
-        print(f"Kullanım Talimatı: {urun_bilgileri[2]}")
-        print(f"Tanıtım: {urun_bilgileri[3]}")
-    else:
-        print("Geçersiz ürün numarası.")
+        print(f"Tanıtım: {urun_bilgileri[2]}")
+
+def urun_duzelt():
+    pass
+
+def urun_sil():
+    pass
+
+
 
 def anamenu():
     print("╔═════════════════════╗")
@@ -52,8 +53,6 @@ def anamenu():
     elif secim == "3": urun_tanit(); anamenu()
     elif secim == "4": urun_duzelt(); anamenu()
     elif secim == "5": urun_sil(); anamenu()
-    elif secim == "6": print("Çıkış yapılıyor...")
-    else: 
-        print("Geçersiz seçim! Tekrar deneyin."); anamenu()
+    elif secim == "6": urun_ekle(); anamenu()
 
 anamenu()
