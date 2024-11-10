b82a420bafb2a85554f40662ee1a47bc609b6f92
Sudekolukisaoglu <sudekolukisaoglu8@gmail.com>
2024-11-10 09:42:07 +0300

    Add files via upload

---
+++ collection2.py
@@ -0,0 +1,14 @@
+rehber={
+    "5236":{
+        "adi":"Rahime SÜZEN",
+        "tel":"5214523652"
+    },
+"5214":{
+    "adi":"Sude AKTÜRK",
+    "tel":"5214525687"
+    }
+}
+# print(rehber)
+print(rehber["5214"]["adi"],rehber["5214"]["tel"])
+rehber["5214"]["adi"] = "Sude AKYOL"
+print(rehber["5214"]["adi"],rehber["5214"]["tel"]) 
