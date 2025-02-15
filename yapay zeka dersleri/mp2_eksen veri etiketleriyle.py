c56a0930ca25461798f1af209bb4f390b7f8452c
sudekolukisaoglu <sudekolukisaoglu8@gmail.com>
2025-02-15 13:18:47 +0300

    python

---
+++ yapay zeka dersleri/mp2_eksen veri etiketleriyle.py
@@ -0,0 +1,7 @@
+#02 Kategori adları
+import matplotlib.pyplot as plt
+plt.plot(["1.Sınav","2.Sınav","3.Sınav"], [80,70,90])
+plt.show()
+
+plt.bar(["1.Sınav","2.Sınav","3.Sınav"], [80,70,90])
+plt.show()
